e35c804f00aeb1f811bf550d6d39bc3a06bb2ed1
Red_Epicness <red.epicness@icloud.com>
2024-12-05 14:33:24 +0100

    Add Copyright header to files

--- installer/src/main/kotlin/gg/essential/installer/launcher/vanilla/MinecraftInstallation.kt
+++ installer/src/main/kotlin/gg/essential/installer/launcher/vanilla/MinecraftInstallation.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2024 ModCore Inc. All rights reserved.
+ *
+ * This code is part of ModCore Inc.’s Essential Installer repository
+ * and is protected under copyright registration #TX0009446119. For the
+ * full license, see:
+ * https://github.com/EssentialGG/EssentialInstaller/blob/main/LICENSE.
+ *
+ * You may modify, create, fork, and use new versions of our Essential
+ * Installer mod in accordance with the GPL-3 License and the additional
+ * provisions outlined in the LICENSE file. You may not sell, license,
+ * commercialize, or otherwise exploit the works in this file or any
+ * other in this repository, all of which is reserved by Essential.
+ */
+
 package gg.essential.installer.launcher.vanilla
 
 import gg.essential.installer.launcher.Installation
