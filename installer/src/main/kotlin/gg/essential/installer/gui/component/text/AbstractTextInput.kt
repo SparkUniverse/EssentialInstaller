9570b57918a99013d371b71eaad49e12eec54596
Callum Bugajski <11320476+CallumBugajski@users.noreply.github.com>
2025-04-05 14:36:46 +0100

    AbstractTextInput: Add new ReplaceTextOperation fix
    Linear: INSTL-182

--- installer/src/main/kotlin/gg/essential/installer/gui/component/text/AbstractTextInput.kt
+++ installer/src/main/kotlin/gg/essential/installer/gui/component/text/AbstractTextInput.kt
@@ -525,7 +525,7 @@ abstract class AbstractTextInput(
             return
         }
 
-        val addTextOperation = AddTextOperation(text, cursor)
+        val addTextOperation = AddTextOperation(text, selectionStart())
 
         if (hasSelection()) {
             val removeTextOperation = RemoveTextOperation(selectionStart(), selectionEnd(), selectAfterUndo = true)
